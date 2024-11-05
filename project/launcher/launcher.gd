7b6ff67c26b5ac2b698fefb67584d3f4fa2d43f8
evankiser01 <evan.kiser@bsu.edu>
2024-11-05 11:09:22 -0500

    Add a restart button to the game
    When the game is over, the button appears, when hit it resets the game

--- project/launcher/launcher.gd
+++ project/launcher/launcher.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
+signal game_over
+
 @export var power := 2000.
 var _current_ball : RigidBody2D = null
 var _balls_left := 1
 
 func _process(_delta: float) -> void:
-
 	if Input.is_action_just_pressed("launch") and _balls_left > 0:
 		if _current_ball == null:
 			var impulse := Vector2(0,-1) * power
@@ -14,3 +15,6 @@ func _process(_delta: float) -> void:
 			_balls_left -= 1
 			_current_ball.global_position = global_position
 			_current_ball.apply_impulse(impulse)
+	
+	if _balls_left == 0 and _current_ball == null:
+		game_over.emit()
--- project/world/world.gd
+++ project/world/world.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-
 func _on_ball_kill_zone_body_entered(_body: Node2D) -> void:
 	_body.queue_free()
 
-func _ready():
-	$GameOver/RestartButton.hide()
-	
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world/world.tscn")
+
+
+func _on_launcher_game_over() -> void:
+	$RestartButton.show()
