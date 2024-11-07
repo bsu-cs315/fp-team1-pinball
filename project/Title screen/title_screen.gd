562e25d3dbe838163f462ce38ddce5b35cf70c53
Paytonh13 <payton.hutsell@gmail.com>
2024-11-07 14:56:24 -0500

    Add Credits and Directions buttons to TitleScreen

--- project/Title screen/title_screen.gd
+++ project/Title screen/title_screen.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
-
-func _on_button_pressed() -> void:
+func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world/world.tscn")
-	
+
+
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_directions_button_pressed() -> void:
+	pass # Replace with function body.
