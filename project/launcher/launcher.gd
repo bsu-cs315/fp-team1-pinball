51a4a54c3949e637ca8fe9e68762844a6afa1457
evankiser01 <evan.kiser@bsu.edu>
2024-11-12 16:38:26 -0500

    Add variation to the power in the launcher

--- project/launcher/launcher.gd
+++ project/launcher/launcher.gd
@@ -5,10 +5,12 @@ extends Node2D
 
 var power := 1500.
 var _current_ball : RigidBody2D = null
+var rng = RandomNumberGenerator.new()
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("launch") and balls_left > 0:
 		if _current_ball == null:
+			power = rng.randf_range(1300,1900)
 			var impulse := Vector2(0,-1) * power
 			_current_ball = preload("res://ball/ball.tscn").instantiate() as RigidBody2D
 			get_parent().add_child(_current_ball)
