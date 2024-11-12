bd05a44b72ebc45862e3b902217f9973982466bc
Evan Lehr <emlehr@bsu.edu>
2024-11-12 15:51:34 -0500

    Make rail obstacles

--- project/bumper/bumper.gd
+++ project/bumper/bumper.gd
@@ -1,12 +1,9 @@
 extends RigidBody2D
 
 
-
-
-
 func _on_bumper_sprite_animation_looped() -> void:
 	$BumperSprite.play("default")
 
 
-func _on_body_entered(body: Node) -> void:
+func _on_body_entered(_body: Node) -> void:
 	$BumperSprite.play("bumper_flash")
