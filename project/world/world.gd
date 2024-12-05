9bcc974c25ff1a0dc951e23c348fbc04169b4bdb
Paytonh13 <payton.hutsell@gmail.com>
2024-12-05 13:02:27 -0500

    Reduce timeout timer

--- project/world/world.gd
+++ project/world/world.gd
@@ -10,7 +10,7 @@ const HIGH_SCORE_FILE: String = "res://high_score.save"
 func _process(_delta: float) -> void:
 	if $Launcher.balls_left == 0 and $Launcher.ball_in_play == false:
 		$GameOverLabel.show()
-		await(get_tree().create_timer(20).timeout)
+		await(get_tree().create_timer(10).timeout)
 		get_tree().change_scene_to_file("res://Title screen/title_screen.tscn")
 	$BallsLeftLabel.text = "Balls Left : %.d" % $Launcher.balls_left
 	_current_score_counter()
