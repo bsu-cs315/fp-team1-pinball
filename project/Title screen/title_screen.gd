a3f6a62c53fc296854a3f80e7f6b9f1a1b9440d1
Paytonh13 <payton.hutsell@gmail.com>
2024-12-05 11:55:18 -0500

    Add High Score to the title screen

--- project/Title screen/title_screen.gd
+++ project/Title screen/title_screen.gd
@@ -1,5 +1,12 @@
 extends CanvasLayer
 
+const HIGH_SCORE_FILE: String = "user://high_score.save"
+
+
+func _ready() -> void:
+	load_and_display_high_score()
+	
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world/world.tscn")
 
@@ -16,3 +23,16 @@ func _on_directions_button_pressed() -> void:
 
 func _on_music_finished() -> void:
 	$Music.play()
+
+
+func load_and_display_high_score() -> void:
+	var high_score = load_high_score()
+	$HighScoreLabel.text = "High Score: %d" % high_score
+	
+func load_high_score() -> int:
+	var file = FileAccess.open(HIGH_SCORE_FILE, FileAccess.READ)
+	if file:
+		var high_score = file.get_32()
+		file.close()
+		return high_score
+	return 0
