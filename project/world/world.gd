24946ff16e5b36c07d385c923c3fe3866d645d55
evankiser01 <evan.kiser@bsu.edu>
2024-12-05 12:18:48 -0500

    Update high score functions

--- project/world/world.gd
+++ project/world/world.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 var _score : int = 0
-var _current_score: int = 0
+# Holds up to 300 score, after 300 gives another ball
+var _current_score: int = 0 
 var _high_score: int = 0
-@export var balls_left := 3
 const HIGH_SCORE_FILE: String = "user://high_score.save"
+
 
 func _process(_delta: float) -> void:
 	if $Launcher.balls_left == 0 and $Launcher.ball_in_play == false:
@@ -13,8 +14,6 @@ func _process(_delta: float) -> void:
 		get_tree().change_scene_to_file("res://Title screen/title_screen.tscn")
 	$BallsLeftLabel.text = "Balls Left : %.d" % $Launcher.balls_left
 	_current_score_counter()
-	update_score
-	
 
 
 func _on_ball_kill_zone_body_entered(body: Node2D) -> void:
@@ -31,28 +30,19 @@ func _on_launcher_zone_body_entered(body: Node2D) -> void:
 
 
 func _on_score_bumper_left_update_score() -> void:
-	_score += 10
-	_current_score += 10
-	_update_score_label()
 	update_score(10)
+	_update_score_label()
 
 
 func _on_score_bumper_right_update_score() -> void:
-	_score += 10
-	_current_score += 10
-	_update_score_label()
 	update_score(10)
+	_update_score_label()
 
 
 func _on_score_bumper_top_update_score() -> void:
-	_score += 10
-	_current_score += 10
-	_update_score_label()
 	update_score(10)
+	_update_score_label()
 
-	
-func _update_score_label() -> void:
-	$ScoreLabel.text = "Score : %.d" % _score
 
 func _current_score_counter() -> void:
 	if _current_score >= 300:
@@ -62,8 +52,8 @@ func _current_score_counter() -> void:
 
 func _ready():
 	load_high_score()
-	update_score_labels()
-	
+	update_high_score_label()
+
 
 func load_high_score():
 	var file = FileAccess.open(HIGH_SCORE_FILE, FileAccess.READ)
@@ -72,20 +62,27 @@ func load_high_score():
 		file.close()
 	else:
 		_high_score = 0
-		
+
+
 func update_score(points: int):
 	_score += points
+	_current_score += points
 	if _score > _high_score:
 		_high_score = _score
 		save_high_score()
-	update_score_labels()
-	
+	update_high_score_label()
+
+
 func save_high_score():
 	var file = FileAccess.open(HIGH_SCORE_FILE,FileAccess.WRITE)
 	if file:
 		file.store_32(_high_score)
 		file.close()
 
-		
-func update_score_labels():
+
+func _update_score_label() -> void:
+	$ScoreLabel.text = "Score : %.d" % _score
+
+
+func update_high_score_label():
 	$HighScoreLabel.text = "High Score: %d" % _high_score
